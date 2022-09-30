672d87b00e4cba15c4ad8ed63cf7ca09bf204c2e
Yadunand Prem <yadunut@gmail.com>
2022-09-30 13:36:06 +0800

    copy files over

---
+++ Lab5/cs2030s/fp/Actionable.java
@@ -0,0 +1,14 @@
+package cs2030s.fp;
+/**
+ * The Actionable interface that can
+ * act when given an action.
+ * Contains a single abstract method act.
+ * CS2030S Lab 4
+ * AY22/23 Semester 1
+ * @author Yadunand Prem (10B)
+ */
+
+// act consumes the T and passes it to action, thus super
+public interface Actionable<T> {
+  public void act(Action<? super T> action);
+}
