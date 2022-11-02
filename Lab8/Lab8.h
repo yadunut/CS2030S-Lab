66ed18d5754dc45431350da890bc72777bbbd2d1
Yadunand Prem <yadunut@gmail.com>
2022-11-02 23:28:05 +0800

    fix lab8.h

--- Lab8/Lab8.h
+++ Lab8/Lab8.h
@@ -13,7 +13,7 @@ mv Constant.java cs2030s/fp/Constant.java
 mv Immutator.java cs2030s/fp/Immutator.java
 mv Immutatorable.java cs2030s/fp/Immutatorable.java
 mv Memo.java cs2030s/fp/Memo.java
-mv Memo.java cs2030s/fp/InfiniteList.java
+mv InfiniteList.java cs2030s/fp/InfiniteList.java
 
 javac cs2030s/fp/*.java
 javac *.java
