538b71a9806fb7a70eeb4aa48e82a08bf551d58d
Yadunand Prem <yadunut@gmail.com>
2022-11-10 16:10:20 +0800

    feat: add lab9

---
+++ cs2030s/Lab9/Lab9b/Streaming.java
@@ -0,0 +1,10 @@
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Streaming {
+  public static <T> List<Pair<Integer, T>> encode(Stream<T> stream) {
+    return null; // Modify this
+  }
+}
